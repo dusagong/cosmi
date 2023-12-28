426b1c6d507cbaafe947c323cd9e505fef43b084
dusagong <yoonseungjae123@gmail.com>
2023-12-28 14:20:30 +0900

    .

--- lib/screen/wish.dart
+++ lib/screen/wish.dart
@@ -48,28 +48,100 @@ class _WishState extends State<Wish> {
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Container(
-            child: Row(
-              children: [
-                Image.asset(
-                  'assets/UT/horse.jpeg',
-                  height: 140,
-                ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.end,
+          // Container(
+          //   child: Row(
+          //     children: [
+          //       Image.asset(
+          //         'assets/UT/horse.jpeg',
+          //         height: 140,
+          //       ),
+          //       Column(
+          //         crossAxisAlignment: CrossAxisAlignment.end,
+          //         children: [
+          //           Text('data'),
+          //           Row(
+          //             mainAxisAlignment: MainAxisAlignment.end,
+          //             children: [
+          //               Container(
+          //                 child: Text('HI'),
+          //               )
+          //             ],
+          //           )
+          //         ],
+          //       )
+          //     ],
+          //   ),
+          // )
+          // Generated code for this Container Widget...
+          Padding(
+            padding: EdgeInsetsDirectional.fromSTEB(16, 8, 16, 0),
+            child: Container(
+              width: double.infinity,
+              height: 100,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                boxShadow: [
+                  BoxShadow(
+                    blurRadius: 4,
+                    color: Color(0x320E151B),
+                    offset: Offset(0, 1),
+                  )
+                ],
+                borderRadius: BorderRadius.circular(8),
+              ),
+              child: Padding(
+                padding: EdgeInsetsDirectional.fromSTEB(12, 8, 8, 8),
+                child: Row(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    Text('data'),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        Container(
-                          child: Text('HI'),
-                        )
-                      ],
-                    )
+                    Hero(
+                      tag: 'ControllerImage',
+                      transitionOnUserGestures: true,
+                      child: ClipRRect(
+                          borderRadius: BorderRadius.circular(12),
+                          child:
+                              // Image.network(
+                              //   'https://static.nike.com/a/images/t_prod_ss/w_640,c_limit,f_auto/95c8dcbe-3d3f-46a9-9887-43161ef949c5/sleepers-of-the-week-release-date.jpg',
+                              //   width: 80,
+                              //   height: 80,
+                              //   fit: BoxFit.fitWidth,
+                              // ),
+                              Image.asset(
+                            'assets/UT/horse.jpeg',
+                            width: 80,
+                            height: 80,
+                            fit: BoxFit.fitWidth,
+                          )),
+                    ),
+                    Expanded(
+                      child: Padding(
+                        padding: EdgeInsetsDirectional.fromSTEB(12, 0, 0, 0),
+                        child: Column(
+                          mainAxisSize: MainAxisSize.max,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Padding(
+                              padding:
+                                  EdgeInsetsDirectional.fromSTEB(0, 0, 0, 8),
+                              child: Text(
+                                '마유 크림',
+                                // style: FlutterFlowTheme.of(context).titleLarge,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    IconButton(
+                        onPressed: () {
+                          // Get.to()
+                        },
+                        icon: Icon(Icons.rocket))
                   ],
-                )
-              ],
+                ),
+              ),
             ),
           )
         ],
